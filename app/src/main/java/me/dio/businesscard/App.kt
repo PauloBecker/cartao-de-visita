fba2d84bf67e75c713b84d33bb5b31df0c020d39
Paulo Becker <beckerpaulo81@gmail.com>
2022-08-07 13:21:05 -0300

    Projeto criado e finalizado

---
+++ app/src/main/java/me/dio/businesscard/App.kt
@@ -0,0 +1,13 @@
+package me.dio.businesscard
+
+import android.app.Application
+import me.dio.businesscard.data.AppDataBase
+import me.dio.businesscard.data.BusinessCardRepository
+
+class App : Application() {
+
+    val database by lazy { AppDataBase.getDatabase(this) }
+
+    val repository by lazy { BusinessCardRepository(database.businessDao()) }
+
+}
